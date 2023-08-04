7a8ee15ed2b1a2da303e2fef6b0269de56f92119
zaigam007 <30535974+zaigam007@users.noreply.github.com>
2023-08-04 14:30:26 +0530

    Add files via upload

---
+++ springboot-demo/springboot-demo/src/test/java/com/staragile/assignment7/springbootdemo/SpringbootDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.staragile.assignment7.springbootdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
